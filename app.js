90d82a5fc99defca65154be4cf5a1fa02c1a4531
Vanja Andrejic <njavagg@gmail.com>
2022-03-14 16:52:42 +0100

    adding slider logic

---
+++ app.js
@@ -0,0 +1,52 @@
+const slider = document.querySelector('.slider_images');
+const sliderImages = document.querySelectorAll('.slider_images img');
+
+const prevBtn = document.querySelector('.slide_left');
+const nextBtn = document.querySelector('.slide_right');
+
+// counter
+
+let counter = 1;
+const size = 950;
+console.log(size);
+
+slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+
+nextBtn.addEventListener('click', ()=> {
+
+    if (counter >= 2) {
+        nextBtn.disable = true;
+    } else { 
+    
+   slider.style.transition = "transform 0.4s ease-in-out";
+   counter++;
+   slider.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+   
+   console.log(counter);
+}
+});
+
+prevBtn.addEventListener('click', ()=> {
+
+    if (counter <= 0) {
+        prevBtn.disable = true;
+    } else { 
+
+    slider.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    slider.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+    
+    console.log(counter);
+    }
+});
+
+
+// slider.addEventListener('transitionend', () => {
+//     if(sliderImages[counter].id === 'lastClone') {
+        
+        
+//         counter = sliderImages.length -2;
+//         slider.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+//     }
+// })
